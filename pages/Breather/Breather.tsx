abfc1c5648a067c5b68918f04ac29dec13d8f0db
NikolasKorakidis <korakidisn@gmail.com>
2020-10-27 17:40:56 +0100

    Feature Done

--- pages/Breather/Breather.tsx
+++ pages/Breather/Breather.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import Animation from "./components/Amination";
 
 export default function Breather() {
-    return (
-        <View>
-            <Text>Hello Nikkkkkko, fix me!</Text>
-        </View>
-    )
+  const [ready, setready] = useState(false);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.baseText}>Take a Breath</Text>
+      <View>{ready ? <Animation /> : <Text>HELLO</Text>}</View>
+      <Button
+        onPress={() => setready(!ready)}
+        title={!ready ? "Click me When you are Ready" : "Go Back"}
+        color="white"
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#292929",
+  },
+  baseText: {
+    fontWeight: "bold",
+    color: "#ffa31a",
+  },
+});
